9184040c9d60d647ab1d1bd0b23933138ddb8454
R3N1CK <renickandrew@gmail.com>
2021-06-05 20:35:45 +0530

    First commit

---
+++ LAB 8_GitHub/server.js
@@ -0,0 +1,18 @@
+var express = require('express'); 
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.use('/images', express.static(__dirname + '/public/images'));
+
+
+//9th program git proile display
+app.get('/',function (req, resp){
+	resp.sendFile(__dirname+"/"+"public/gitppro.html")
+});
+
+
+app.listen(1234,function(){
+	console.log('Listening at port 1234');
+});
